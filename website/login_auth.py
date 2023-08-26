3dd5fa573b8b3a8dfd80efa847eae363ea35e52b
keiransystem14 <keiran1412@hotmail.com>
2023-08-26 16:00:41 +0100

    Entered the if statements to ensure the user is able to login to the website and redirects the user to the homepage.

--- website/login_auth.py
+++ website/login_auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from website import app_db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 login_auth = Blueprint('login_auth', __name__)
 
@@ -22,7 +23,21 @@ Flash library is used to flash an error message when the user doesn't meet the s
 
 @login_auth.route('/login', methods=['GET', 'POST'])
 def login(): 
-    return render_template("login.html", text="Testing")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                
+                return redirect(url_for('web_design.home'))
+            else:
+                flash('Incorrect password, please try again', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    return render_template("login.html", boolean=True)
 
 @login_auth.route('/logout')
 def logout():
@@ -36,7 +51,11 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters', category='error')
         elif len(first_name) < 2:
             flash('The firstname must be greater than 1 characters', category='error')
