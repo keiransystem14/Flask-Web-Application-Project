09f30eaf8d1da82afe2083425f519fbbd1a729cf
keiransystem14 <keiran1412@hotmail.com>
2023-08-26 12:30:18 +0100

    Defined the logic of relating the user accounts to the notes that the user has made inside the models.py file

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize Database
+
+app_db = SQLAlchemy()
+DB_NAME = "account.db"
+
+
 
 def create_application():
     application = Flask(__name__)  #Represents the name of the file.
     application.config['SECRET_KEY'] = 'DKJSDKFJSLDKFKJDKJF'
+    application.config['SQLAlchemy_DATABASE_URI'] = f'sqlite://{DB_NAME}'
+
+    app_db.init_app(application)
     
     from .web_design import web_design
     from .login_auth import login_auth
