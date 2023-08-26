069e24e70d72cf21e411362e8e7b690998b7b84a
keiransystem14 <keiran1412@hotmail.com>
2023-08-26 13:15:17 +0100

    I have now fixed the Invalid SQLite syntax. Going to test it to ensure it works.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from website import create_application
 
-application = create_application()
+app = create_application()
 
-if __name__ == '__main__':
-    application.run(debug=True) # If any changes were made to python code, it will automatically re-run the server. This is for staging.
+if __name__ == 'a__main__':
+    app.run(debug=True) # If any changes were made to python code, it will automatically re-run the server. This is for staging.
--- website/__init__.py
+++ website/__init__.py
@@ -9,16 +9,16 @@ DB_NAME = "account.db"
 
 
 def create_application():
-    application = Flask(__name__)  #Represents the name of the file.
-    application.config['SECRET_KEY'] = 'DKJSDKFJSLDKFKJDKJF'
-    application.config['SQLAlchemy_DATABASE_URI'] = f'sqlite://{DB_NAME}'
+    app = Flask(__name__)  #Represents the name of the file.
+    app.config['SECRET_KEY'] = 'DKJSDKFJSLDKFKJDKJF'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #Incorrect SQLite link which is corrected to sqlite:///{DB_NAME}
 
-    app_db.init_app(application)
+    app_db.init_app(app)
     
     from .web_design import web_design
     from .login_auth import login_auth
 
-    application.register_blueprint(web_design, url_prefix='/')
-    application.register_blueprint(login_auth, url_prefix='/')
+    app.register_blueprint(web_design, url_prefix='/')
+    app.register_blueprint(login_auth, url_prefix='/')
 
-    return application
+    return app
