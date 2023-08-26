10cbe1dfb05ba0b76f4da368e0136f82d9167cd2
keiransystem14 <keiran1412@hotmail.com>
2023-08-26 13:21:49 +0100

    Another syntax error where the character a 'a__main__' in main was mistyped or misplaced.

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from website import create_application
 
 app = create_application()
 
-if __name__ == 'a__main__':
+if __name__ == '__main__':
     app.run(debug=True) # If any changes were made to python code, it will automatically re-run the server. This is for staging.
